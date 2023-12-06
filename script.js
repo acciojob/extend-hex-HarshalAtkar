82cd7f5910f3ef217945dd2ef73de07e2f156e7e
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-06 23:35:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let newstring;
+	for(i of hortHex){
+		if(shortHex(i)!=#){
+			newstring=shortHex(i)+shortHex(i);
+		}
+		return newstring;
+	}
 };
 
 // Do not change the code below.
