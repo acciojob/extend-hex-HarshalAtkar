8a6e10517662768a673294a82a97bc7613f191fe
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-08 22:51:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+// const extendHex = (shortHex) => {
+//   // write your code here
+// 	let newstring;
+// 	for(let i of shortHex){
+// 		if(shortHex(i)!=#){
+// 			newstring=newstring+shortHex(i)+shortHex(i);
+// 		}
+// 		return #+newstring;
+// 	}
+// };
 const extendHex = (shortHex) => {
-  // write your code here
-	let newstring;
-	for(let i of shortHex){
-		if(shortHex(i)!=#){
-			newstring=newstring+shortHex(i)+shortHex(i);
-		}
-		return #+newstring;
-	}
+  let newstring = "";
+  for(let i of shortHex){
+    if(i!="#"){
+      newstring=newstring+i+i;
+    }
+  }
+  return "#"+newstring;
 };
+
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
